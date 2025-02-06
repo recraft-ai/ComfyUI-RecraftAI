f35b5769304e7819e4c20ec2489e6dbfc1bc3ea1
Ivan Safonov <isafonov27@gmail.com>
2025-02-06 15:44:28 +0000

    feat: add Image to Image, Replace Background, Inpaint nodes (#4)

--- py/nodes.py
+++ py/nodes.py
@@ -1,23 +1,32 @@
 from .recraft_nodes import (
     Client,
     ImageGenerator,
+    ImageToImageTransformer,
     BackgroundRemover,
-    ClarityUpscaler,
-    GenerativeUpscaler,
+    CrispUpscaler,
+    CreativeUpscaler,
+    BackgroundReplacer,
+    Inpainter,
 )
 
 NODE_CLASS_MAPPINGS = {
     'RecraftClient': Client,
     'RecraftImageGenerator': ImageGenerator,
+    'RecraftImageToImageTransformer': ImageToImageTransformer,
     'RecraftBackgroundRemover': BackgroundRemover,
-    'RecraftClarityUpscaler': ClarityUpscaler,
-    'RecraftGenerativeUpscaler': GenerativeUpscaler,
+    'RecraftClarityUpscaler': CrispUpscaler,
+    'RecraftGenerativeUpscaler': CreativeUpscaler,
+    'RecraftBackgroundReplacer': BackgroundReplacer,
+    'RecraftInpainter': Inpainter,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     'RecraftClient': 'RecraftAI Client',
     'RecraftImageGenerator': 'RecraftAI Image Generator',
+    'RecraftImageToImageTransformer': 'RecraftAI Image To Image Transformer',
     'RecraftBackgroundRemover': 'RecraftAI Background Remover',
-    'RecraftClarityUpscaler': 'RecraftAI Clarity Upscaler',
-    'RecraftGenerativeUpscaler': 'RecraftAI Generative Upscaler',
+    'RecraftClarityUpscaler': 'RecraftAI Crisp Upscaler',
+    'RecraftGenerativeUpscaler': 'RecraftAI Creative Upscaler',
+    'RecraftBackgroundReplacer': 'RecraftAI Background Replacer',
+    'RecraftInpainter': 'RecraftAI Inpainter',
 }
--- py/recraft_nodes.py
+++ py/recraft_nodes.py
@@ -17,6 +17,59 @@ config = configparser.ConfigParser()
 config.read(config_path)
 
 
+STYLE_LIST = [
+    '',
+    'any',
+    'realistic_image',
+    'digital_illustration'
+]
+SUBSTYLE_LIST = [
+    '',
+    'b_and_w',
+    'hard_flash',
+    'hdr',
+    'natural_light',
+    'studio_portrait',
+    'enterprise',
+    'motion_blur',
+    'pixel_art',
+    'hand_drawn',
+    'grain',
+    'infantile_sketch',
+    '2d_art_poster',
+    'handmade_3d',
+    'hand_drawn_outline',
+    'engraving_color',
+    '2d_art_poster_2',
+]
+IMAGE_SIZE_LIST = [
+    '',
+    '1024x1024',
+    '1024x1280',
+    '1024x1365',
+    '1024x1434',
+    '1024x1536',
+    '1024x1707',
+    '1024x1820',
+    '1024x2048',
+    '1280x1024',
+    '1365x1024',
+    '1434x1024',
+    '1536x1024',
+    '1707x1024',
+    '1820x1024',
+    '2048x1024',
+]
+SEED_CONFIGURATION = ('INT', {
+    'default': 0,
+    'min': 0,
+    'max': 2147483647,
+    'step': 1,
+    'display': 'number',
+    'lazy': True
+})
+
+
 try:
     recraft_api_token = config['API']['RECRAFT_API_TOKEN']
     if recraft_api_token != '':
@@ -39,10 +92,14 @@ def _make_tensor(image_data):
         return tensor
 
 
-def _make_image_data(tensor):
+def _make_image_data(tensor, is_mask=False):
     tensor_np = tensor[0].numpy()
     if tensor_np.dtype == numpy.float32 or tensor_np.dtype == numpy.float64:
         tensor_np = (tensor_np * 255).clip(0, 255).astype(numpy.uint8)
+
+    if is_mask:
+        # we do not force the mask to be binary, but we do threshold it
+        tensor_np = numpy.where(tensor_np >= 127.5, 255, 0).astype(numpy.uint8)
 
     image = Image.fromarray(tensor_np)
 
@@ -76,28 +133,56 @@ class RecraftClient:
             raise ValueError(data.get('message', 'empty error message'))
         return data['data'][0]['url']
 
-    def __process_image(self, operation, image_data, random_seed=None):
-        with io.BytesIO(image_data) as fp:
-            response = requests.post(
-                self._BASE_URL + f'/images/{operation}',
-                headers={'Authorization': f'Bearer {self._token}'},
-                json={'random_seed': random_seed or None},
-                files={'file': fp},
-            )
+    def __process_image(self, operation, image_data, mask_data=None, params=None, random_seed=None):
+        if params is None:
+            params = {}
+        if random_seed is not None:
+            params['random_seed'] = random_seed
+
+        with io.BytesIO(image_data) as image_fp:
+            if mask_data is None:
+                response = requests.post(
+                    self._BASE_URL + f'/images/{operation}',
+                    headers={'Authorization': f'Bearer {self._token}'},
+                    data=params,
+                    files={'image': image_fp},
+                )
+            else:
+                with io.BytesIO(mask_data) as mask_fp:
+                    response = requests.post(
+                        self._BASE_URL + f'/images/{operation}',
+                        headers={'Authorization': f'Bearer {self._token}'},
+                        data=params,
+                        files={'image': image_fp, 'mask': mask_fp},
+                    )
 
         data = response.json()
+        
         if 'code' in data:
             raise ValueError(data.get('message', 'empty error message'))
-        return data['image']['url']
+
+        if 'image' in data:
+            return data['image']['url']
+        else:
+            return data['data'][0]['url']
+
+    def image_to_image(self, image_data, params, random_seed=None):
+        return self.__process_image('imageToImage', image_data, params=params, random_seed=random_seed)
 
     def remove_background(self, image_data, random_seed=None):
         return self.__process_image('removeBackground', image_data, random_seed=random_seed)
 
-    def generative_upscale(self, image_data, random_seed=None):
-        return self.__process_image('generativeUpscale', image_data, random_seed=random_seed)
+    def creative_upscale(self, image_data, random_seed=None):
+        return self.__process_image('creativeUpscale', image_data, random_seed=random_seed)
 
-    def clarity_upscale(self, image_data, random_seed=None):
-        return self.__process_image('clarityUpscale', image_data, random_seed=random_seed)
+    def crisp_upscale(self, image_data, random_seed=None):
+        return self.__process_image('crispUpscale', image_data, random_seed=random_seed)
+    
+    def replace_background(self, image_data, params, random_seed=None):
+        return self.__process_image('replaceBackground', image_data, params=params, random_seed=random_seed)
+    
+    def inpaint(self, image_data, mask_data, params, random_seed=None):
+        return self.__process_image('inpaint', image_data, mask_data=mask_data, params=params, random_seed=random_seed)
 
 
 class Client:
@@ -146,62 +231,20 @@ class ImageGenerator:
                 'prompt': ('STRING', {'multiline': True, 'default': ''}),
             },
             'optional': {
-                'style': (['', 'any', 'realistic_image', 'digital_illustration'],),
-                'substyle': ([
-                    '',
-                    'b_and_w',
-                    'hard_flash',
-                    'hdr',
-                    'natural_light',
-                    'studio_portrait',
-                    'enterprise',
-                    'motion_blur',
-                    'pixel_art',
-                    'hand_drawn',
-                    'grain',
-                    'infantile_sketch',
-                    '2d_art_poster',
-                    'handmade_3d',
-                    'hand_drawn_outline',
-                    'engraving_color',
-                    '2d_art_poster_2',
-                ],),
-                'image_size': ([
-                    '',
-                    '1024x1024',
-                    '1024x1280',
-                    '1024x1365',
-                    '1024x1434',
-                    '1024x1536',
-                    '1024x1707',
-                    '1024x1820',
-                    '1024x2048',
-                    '1280x1024',
-                    '1365x1024',
-                    '1434x1024',
-                    '1536x1024',
-                    '1707x1024',
-                    '1820x1024',
-                    '2048x1024',
-                ],),
+                'style': (STYLE_LIST,),
+                'substyle': (SUBSTYLE_LIST,),
+                'image_size': (IMAGE_SIZE_LIST,),
                 'model': ([
                     '',
                     'recraftv3',
                     'recraft20b',
                 ],),
-                'seed': ('INT', {
-                    'default': 0,
-                    'min': 0,
-                    'max': 2147483647,
-                    'step': 1,
-                    'display': 'number',
-                    'lazy': True
-                }),
+                'seed': SEED_CONFIGURATION,
             },
         }
 
     '''
-    Generate an image given a prompt
+    Generate an image given a text prompt
     '''
     def generate(self, client, prompt, style, substyle, image_size, model, seed):
         if prompt == '':
@@ -216,6 +259,53 @@ class ImageGenerator:
             random_seed=seed,
         )
         print('Generated image', image_url)
+
+        image_data = _fetch_image(image_url)
+        tensor = _make_tensor(image_data)
+        return (tensor,)
+
+
+class ImageToImageTransformer:
+    CATEGORY = 'RecraftAI'
+    FUNCTION = 'image_to_image'
+    RETURN_TYPES = ('IMAGE',)
+    RETURN_NAMES = ('image',)
+    OUTPUT_NODE = True
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            'required': {
+                'client': ('RECRAFTCLIENT', {'forceInput': True}),
+                'image': ('IMAGE', {'forceInput': True}),
+                'prompt': ('STRING', {'multiline': True, 'default': ''}),
+                'strength': ('FLOAT', {'default': 0.5, 'min': 0.0, 'max': 1.0, 'step': 0.01}),
+            },
+            'optional': {
+                'style': (STYLE_LIST,),
+                'substyle': (SUBSTYLE_LIST,),
+                'seed': SEED_CONFIGURATION,
+            },
+        }
+
+    '''
+    Transform an input image into an output image given a text prompt
+    '''
+    def image_to_image(self, client, image, prompt, strength, style, substyle, seed):
+        if prompt == '':
+            raise ValueError('Prompt is required')
+
+        image_url = client.image_to_image(
+            _make_image_data(image),
+            {
+                'prompt': prompt,
+                'strength': strength,
+                'style': style or None,
+                'substyle': substyle or None,
+            },
+            random_seed=seed,
+        )
+        print('Image To Image finished', image_url)
 
         image_data = _fetch_image(image_url)
         tensor = _make_tensor(image_data)
@@ -237,13 +327,7 @@ class BackgroundRemover:
                 'image': ('IMAGE', {'forceInput': True}),
             },
             'optional': {
-                'seed': ('INT', {
-                    'default': 0,
-                    'min': 0,
-                    'max': 2147483647,
-                    'step': 1,
-                    'display': 'number',
-                }),
+                'seed': SEED_CONFIGURATION,
             },
         }
 
@@ -259,9 +343,9 @@ class BackgroundRemover:
         return (tensor,)
 
 
-class ClarityUpscaler:
+class CrispUpscaler:
     CATEGORY = 'RecraftAI'
-    FUNCTION = 'clarity_upscale'
+    FUNCTION = 'crisp_upscale'
     RETURN_TYPES = ('IMAGE',)
     RETURN_NAMES = ('image',)
     OUTPUT_NODE = True
@@ -285,20 +369,20 @@ class ClarityUpscaler:
         }
 
     '''
-    Clarity upscale of the given image
+    Crisp upscale of the given image
     '''
-    def clarity_upscale(self, client, image, seed):
-        image_url = client.clarity_upscale(_make_image_data(image), random_seed=seed)
-        print('Clarity upscale finished', image_url)
+    def crisp_upscale(self, client, image, seed):
+        image_url = client.crisp_upscale(_make_image_data(image), random_seed=seed)
+        print('Crisp upscale finished', image_url)
 
         image_data = _fetch_image(image_url)
         tensor = _make_tensor(image_data)
         return (tensor,)
 
 
-class GenerativeUpscaler:
+class CreativeUpscaler:
     CATEGORY = 'RecraftAI'
-    FUNCTION = 'generative_upscale'
+    FUNCTION = 'creative_upscale'
     RETURN_TYPES = ('IMAGE',)
     RETURN_NAMES = ('image',)
     OUTPUT_NODE = True
@@ -311,22 +395,108 @@ class GenerativeUpscaler:
                 'image': ('IMAGE', {'forceInput': True}),
             },
             'optional': {
-                'seed': ('INT', {
-                    'default': 0,
-                    'min': 0,
-                    'max': 2147483647,
-                    'step': 1,
-                    'display': 'number',
-                }),
+                'seed': SEED_CONFIGURATION,
             },
         }
 
     '''
-    Generative upscale of the given image
+    Creative upscale of the given image
     '''
-    def generative_upscale(self, client, image, seed):
-        image_url = client.generative_upscale(_make_image_data(image), random_seed=seed)
-        print('Generative upscale finished', image_url)
+    def creative_upscale(self, client, image, seed):
+        image_url = client.creative_upscale(_make_image_data(image), random_seed=seed)
+        print('Creative upscale finished', image_url)
+
+        image_data = _fetch_image(image_url)
+        tensor = _make_tensor(image_data)
+        return (tensor,)
+
+
+class BackgroundReplacer:
+    CATEGORY = 'RecraftAI'
+    FUNCTION = 'replace_background'
+    RETURN_TYPES = ('IMAGE',)
+    RETURN_NAMES = ('image',)
+    OUTPUT_NODE = True
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            'required': {
+                'client': ('RECRAFTCLIENT', {'forceInput': True}),
+                'image': ('IMAGE', {'forceInput': True}),
+                'prompt': ('STRING', {'multiline': True, 'default': ''}),
+            },
+            'optional': {
+                'style': (STYLE_LIST,),
+                'substyle': (SUBSTYLE_LIST,),
+                'seed': SEED_CONFIGURATION,
+            },
+        }
+
+    '''
+    Replace image background based on a given prompt
+    '''
+    def replace_background(self, client, image, prompt, style, substyle, seed):
+        if prompt == '':
+            raise ValueError('Prompt is required')
+
+        image_url = client.replace_background(
+            _make_image_data(image),
+            {
+                'prompt': prompt,
+                'style': style or None,
+                'substyle': substyle or None,
+            },
+            random_seed=seed,
+        )
+        print('Replace background finished', image_url)
+
+        image_data = _fetch_image(image_url)
+        tensor = _make_tensor(image_data)
+        return (tensor,)
+
+
+class Inpainter:
+    CATEGORY = 'RecraftAI'
+    FUNCTION = 'inpaint'
+    RETURN_TYPES = ('IMAGE',)
+    RETURN_NAMES = ('image',)
+    OUTPUT_NODE = True
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            'required': {
+                'client': ('RECRAFTCLIENT', {'forceInput': True}),
+                'image': ('IMAGE', {'forceInput': True}),
+                'mask': ('MASK', {'forceInput': True}),
+                'prompt': ('STRING', {'multiline': True, 'default': ''}),
+            },
+            'optional': {
+                'style': (STYLE_LIST,),
+                'substyle': (SUBSTYLE_LIST,),
+                'seed': SEED_CONFIGURATION,
+            },
+        }
+
+    '''
+    Inpaint an image given mask and prompt
+    '''
+    def inpaint(self, client, image, mask, prompt, style, substyle, seed):
+        if prompt == '':
+            raise ValueError('Prompt is required')
+
+        image_url = client.inpaint(
+            _make_image_data(image),
+            _make_image_data(mask, is_mask=True),
+            {
+                'prompt': prompt,
+                'style': style or None,
+                'substyle': substyle or None,
+            },
+            random_seed=seed,
+        )
+        print('Inpaint finished', image_url)
 
         image_data = _fetch_image(image_url)
         tensor = _make_tensor(image_data)
